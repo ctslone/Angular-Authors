a9bff43024bf3a987c1a5427faaaa1a6ac78c86d
Charlie <ctslone27@gmail.com>
2020-03-31 18:33:05 -0400

    refactor to use private methods and clean up the code logic in the transform.

--- src/app/form-pipe.pipe.ts
+++ src/app/form-pipe.pipe.ts
@@ -11,18 +11,27 @@ export class FormPipePipe implements PipeTransform {
     }
     // separating all words with a space
     let words = value.split(' ');
-    let preps = ["the", "or", "an", "and", "of"];
+    
     // loop through all the words and if a prep is found, only after the first word, make sure it is lower case. otherwide capitalize the first letter of all other words
     for (var i=0; i<words.length; i++) {
-      if (i > 0 && preps.includes( words[i].toLowerCase() )) {
+      if (i > 0 && this.isPrep(words[i])) {
         words[i] = words[i].toLowerCase()
       }
       else {
-        words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1).toLowerCase()
+        words[i] = this.notPrep(words[i])
       }
     }
     // return the result to the user
     return words.join(' ')
+  }
+
+  private isPrep(word) {
+    let preps = ["the", "or", "an", "and", "of"];
+    return preps.includes( word.toLowerCase() )
+  }
+
+  private notPrep(word) {
+    return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
   }
 
 }
